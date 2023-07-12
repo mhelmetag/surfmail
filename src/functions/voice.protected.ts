34e25ea7ce81edc519b2835996c521d35c8fcc9d
Max Helmetag <mhelmetag@gmail.com>
2023-07-11 23:18:23 -0700

    predeploy scripts

--- src/functions/voice.protected.ts
+++ src/functions/voice.protected.ts
@@ -1,7 +1,12 @@
 import * as Twilio from "twilio";
 
 exports.handler = function (_context, _event, callback) {
+  const assetsBaseURL = "https://turquoise-emu-1178.twil.io/assets";
+  const now = new Date();
+  const assetURL = `${assetsBaseURL}/surfmail-${now.getFullYear()}-${now.getDate()}-${now.getDay()}.mpeg`;
+
   const twiml = new Twilio.twiml.VoiceResponse();
-  twiml.play("https://turquoise-emu-1178.twil.io/assets/recording.mpeg");
+  twiml.play(assetURL);
+
   callback(null, twiml);
 };
